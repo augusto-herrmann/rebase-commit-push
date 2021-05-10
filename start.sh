e4def41af2515462773852161c008ac633969a95
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2021-05-10 21:28:48 +1000

    Use GITHUB_TOKEN

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
 echo "Push to branch $INPUT_BRANCH";
-[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+[ -z "${GITHUB_TOKEN}" ] && {
     echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
     exit 1;
 };
@@ -36,7 +36,7 @@ fi
 
 cd "${INPUT_DIRECTORY}"
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
 git config http.sslVerify false
 git config --local user.email "${INPUT_AUTHOR_EMAIL}"
